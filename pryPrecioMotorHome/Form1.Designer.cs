c80e952e598934565074e1392b6cbab3b299ddae
Facundo Lopez Vital <facu_lv_13@hotmail.com>
2024-10-26 19:02:44 -0300

    Agregar archivos de proyecto.

---
+++ pryPrecioMotorHome/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace pryPrecioMotorHome
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chbEquipo = new System.Windows.Forms.CheckBox();
+            this.chbInternet = new System.Windows.Forms.CheckBox();
+            this.chbTelevision = new System.Windows.Forms.CheckBox();
+            this.cmbModelo = new System.Windows.Forms.ComboBox();
+            this.cmbPersonas = new System.Windows.Forms.ComboBox();
+            this.lblError = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rdbSi = new System.Windows.Forms.RadioButton();
+            this.rdbNo = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(179, 212);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(84, 39);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(282, 212);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(84, 39);
+            this.btnLimpiar.TabIndex = 1;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtResultado.Enabled = false;
+            this.txtResultado.Location = new System.Drawing.Point(214, 303);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(120, 22);
+            this.txtResultado.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(216, 274);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "El total a pagar es:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "EXTRAS";
+            // 
+            // chbEquipo
+            // 
+            this.chbEquipo.AutoSize = true;
+            this.chbEquipo.Location = new System.Drawing.Point(13, 212);
+            this.chbEquipo.Name = "chbEquipo";
+            this.chbEquipo.Size = new System.Drawing.Size(136, 20);
+            this.chbEquipo.TabIndex = 5;
+            this.chbEquipo.Text = "Equipo de Cocina";
+            this.chbEquipo.UseVisualStyleBackColor = true;
+            this.chbEquipo.CheckedChanged += new System.EventHandler(this.chbEquipo_CheckedChanged);
+            // 
+            // chbInternet
+            // 
+            this.chbInternet.AutoSize = true;
+            this.chbInternet.Location = new System.Drawing.Point(12, 253);
+            this.chbInternet.Name = "chbInternet";
+            this.chbInternet.Size = new System.Drawing.Size(72, 20);
+            this.chbInternet.TabIndex = 6;
+            this.chbInternet.Text = "Internet";
+            this.chbInternet.UseVisualStyleBackColor = true;
+            this.chbInternet.CheckedChanged += new System.EventHandler(this.chbInternet_CheckedChanged);
+            // 
+            // chbTelevision
+            // 
+            this.chbTelevision.AutoSize = true;
+            this.chbTelevision.Location = new System.Drawing.Point(12, 293);
+            this.chbTelevision.Name = "chbTelevision";
+            this.chbTelevision.Size = new System.Drawing.Size(92, 20);
+            this.chbTelevision.TabIndex = 7;
+            this.chbTelevision.Text = "Television";
+            this.chbTelevision.UseVisualStyleBackColor = true;
+            this.chbTelevision.CheckedChanged += new System.EventHandler(this.chbTelevision_CheckedChanged);
+            // 
+            // cmbModelo
+            // 
+            this.cmbModelo.FormattingEnabled = true;
+            this.cmbModelo.Items.AddRange(new object[] {
+            "Modelo A",
+            "Modelo B",
+            "Modelo C"});
+            this.cmbModelo.Location = new System.Drawing.Point(39, 43);
+            this.cmbModelo.Name = "cmbModelo";
+            this.cmbModelo.Size = new System.Drawing.Size(144, 24);
+            this.cmbModelo.TabIndex = 8;
+            this.cmbModelo.SelectedIndexChanged += new System.EventHandler(this.cmbModelo_SelectedIndexChanged);
+            // 
+            // cmbPersonas
+            // 
+            this.cmbPersonas.FormattingEnabled = true;
+            this.cmbPersonas.Location = new System.Drawing.Point(222, 43);
+            this.cmbPersonas.Name = "cmbPersonas";
+            this.cmbPersonas.Size = new System.Drawing.Size(144, 24);
+            this.cmbPersonas.TabIndex = 9;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Location = new System.Drawing.Point(39, 13);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 16);
+            this.lblError.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(127, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "¿Desea agregar seguro?";
+            // 
+            // rdbSi
+            // 
+            this.rdbSi.AutoSize = true;
+            this.rdbSi.Location = new System.Drawing.Point(160, 123);
+            this.rdbSi.Name = "rdbSi";
+            this.rdbSi.Size = new System.Drawing.Size(40, 20);
+            this.rdbSi.TabIndex = 12;
+            this.rdbSi.TabStop = true;
+            this.rdbSi.Text = "Si";
+            this.rdbSi.UseVisualStyleBackColor = true;
+            // 
+            // rdbNo
+            // 
+            this.rdbNo.AutoSize = true;
+            this.rdbNo.Location = new System.Drawing.Point(160, 149);
+            this.rdbNo.Name = "rdbNo";
+            this.rdbNo.Size = new System.Drawing.Size(46, 20);
+            this.rdbNo.TabIndex = 13;
+            this.rdbNo.TabStop = true;
+            this.rdbNo.Text = "No";
+            this.rdbNo.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(396, 355);
+            this.Controls.Add(this.rdbNo);
+            this.Controls.Add(this.rdbSi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.cmbPersonas);
+            this.Controls.Add(this.cmbModelo);
+            this.Controls.Add(this.chbTelevision);
+            this.Controls.Add(this.chbInternet);
+            this.Controls.Add(this.chbEquipo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Calculador de Precios MotorHome";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chbEquipo;
+        private System.Windows.Forms.CheckBox chbInternet;
+        private System.Windows.Forms.CheckBox chbTelevision;
+        private System.Windows.Forms.ComboBox cmbModelo;
+        private System.Windows.Forms.ComboBox cmbPersonas;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton rdbSi;
+        private System.Windows.Forms.RadioButton rdbNo;
+    }
+}
+
---
+++ pryPrecioMotorHome/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryPrecioMotorHome
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int acumulador = 0;
+        int total = 0;
+        int personas = 0;
+        int extras = 0;
+
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {           
+            if (cmbModelo.Text != "")
+            {                
+                switch (cmbModelo.SelectedIndex)
+                {
+                    case 0:
+                        acumulador = 1000;
+                        total += acumulador;
+                        break;
+                    case 1:
+                        acumulador = 1500;
+                        total += acumulador;
+                        break;
+                    default:
+                        acumulador = 2000;
+                        total += acumulador;
+                        break;
+                }
+
+                lblError.Text = "";
+            }
+            else
+            {
+                lblError.Text = "Error, debe seleccionar un modelo para continuar";
+            }
+
+            if (rdbSi.Checked)
+            {             
+                 txtResultado.Text = (total + 500).ToString();
+                total = Convert.ToInt32(txtResultado.Text);
+            }
+
+            personas = Convert.ToInt32(cmbPersonas.Text);
+            txtResultado.Text = ((personas * 100) + total + extras).ToString();
+            total = 0;
+        }
+
+        private void cmbModelo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int limite = 0;
+            switch (cmbModelo.SelectedItem)
+            {
+                case "Modelo A":
+                    cmbPersonas.Items.Clear();
+                    cmbPersonas.Text = string.Empty;
+                    limite = 4;
+                    break;
+                case "Modelo B":
+                    cmbPersonas.Items.Clear();
+                    cmbPersonas.Text = string.Empty;
+                    limite = 6;
+                    break;
+                default:
+                    cmbPersonas.Items.Clear();
+                    cmbPersonas.Text = string.Empty;
+                    limite = 9;
+                    break;
+            }
+            for (int i = 1; i < limite; i++)
+            {
+                cmbPersonas.Items.Add(i);
+            }            
+        }
+        public void Extras(bool valor)
+        {
+            if (valor)
+            {
+                extras += 50;
+            } else
+            {
+                extras -= 50;
+            }
+        }
+        private void chbEquipo_CheckedChanged(object sender, EventArgs e)
+        {
+            Extras(chbEquipo.Checked);
+        }
+        private void chbInternet_CheckedChanged(object sender, EventArgs e)
+        {
+            Extras(chbInternet.Checked);
+        }
+        private void chbTelevision_CheckedChanged(object sender, EventArgs e)
+        {
+            Extras(chbTelevision.Checked);
+        }
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            rdbSi.Checked = false;
+            rdbNo.Checked = false;
+            chbEquipo.Checked = false;
+            chbInternet.Checked = false;
+            chbTelevision.Checked = false;
+            txtResultado.Text = "";
+            cmbModelo.Text = "";
+            cmbPersonas.Text = "";
+            lblError.Text = "";
+        }
+    }
+}
